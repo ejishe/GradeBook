1b6db7b6f324d6c67bb5993ad1387114b7f44703
Ислам <islam@MacBook-Pro-Islam.local>
2026-01-06 21:53:56 +0300

    all models

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from journal.models import *
+
+
+@admin.register(Group, Student, Subject, Lesson, LessonResult)
+class GroupAdmin(admin.ModelAdmin):
+    pass
--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    lesson_types = [
+        ('lecture', 'Лекция'),
+        ('practice', 'Практика')
+    ]
+
+    date = models.DateField(auto_now_add=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='lessons')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='lessons')
+    lesson_type = models.CharField(max_length=20, choices=lesson_types)
+
+    def __str__(self):
+        return str(self.subject)
+
+
+class LessonResult(models.Model):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name='results'
+    )
+
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        related_name='results'
+    )
+
+    attended = models.BooleanField(default=False)
+
+    practice_score = models.IntegerField(null=True, blank=True)
+    lab_score = models.IntegerField(null=True, blank=True)
+    homework_score = models.IntegerField(null=True, blank=True)
+    card_score = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('student', 'lesson')
